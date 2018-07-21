8b5a5bb06668f3743788704db6eb6d58ca1c6d67
motio <greatmotio@gmail.com>
2018-07-21 12:23:29 +0900

    Almost finished

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def example
+  end
+
+  def function
+  end
+
+  def about
+  end
+
+  def howto
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+	devise_for :users
+	root "pages#index"
+	get '/pages/show', to: 'pages#show'
+	get '/howto', to: 'pages#howto'
+	get '/about', to: 'pages#about'
+	resources :users
+	resources :posts
+	get 'inquiry' => 'inquiry#index'              # 入力画面
+	post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+	post 'inquiry/thanks' => 'inquiry#thanks'
+	get 'contact', to: 'inquiry#contact'
+	get 'function', to: 'pages#function'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
